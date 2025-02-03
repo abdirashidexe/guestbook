dc449187d62ed0e87ee68254d07816ccd8afd568
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-02-03 01:49:44 -0800

    app.js complete, css next

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({extended: true}));
+
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -13,4 +15,10 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.post('/thankyou', (req, res) => {
+
+    res.sendFile(`${import.meta.dirname}/views/thankyou.html`);
+    console.log(req.body);
 });
